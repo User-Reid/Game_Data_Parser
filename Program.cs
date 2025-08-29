be11dc295f08748db7af051f3196ba13f34c08a9
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-28 22:24:07 -0500

    ok made some real progess... sadly took as long as it did...

--- Program.cs
+++ Program.cs
@@ -1,6 +1,52 @@
-﻿using Game_Data_Parser.App;
-using Game_Data_Parser.VideoGameDirectory;
+﻿var _videoGameDirectory = new VideoGameDirectory();
+var _application = new App(_videoGameDirectory.GameDirectory);
 
-Run(new GameDirectory(), new GameRepository());
+_application.Run();
 
 Console.ReadKey();
+
+public class App
+{
+  public List<VideoGame> GameDirectory { get; }
+
+  public App(List<VideoGame> gameDirectory)
+  {
+    GameDirectory = gameDirectory;
+  }
+
+  public void Run()
+  {
+    var writeToDirectory = new WriteToDirectory();
+
+    writeToDirectory.Write(GameDirectory);
+
+
+  }
+}
+public class VideoGameDirectory
+{
+  public List<VideoGame> GameDirectory { get; } = new List<VideoGame>();
+}
+
+public class VideoGame
+{
+  public string Name { get; }
+  public int ReleaseYear { get; }
+  public double Rating { get; }
+
+  public VideoGame(string name, int releaseYear, double rating)
+  {
+    Name = name;
+    ReleaseYear = releaseYear;
+    Rating = rating;
+  }
+}
+
+public class WriteToDirectory
+{
+  public void Write(List<VideoGame> gameDirectory)
+  {
+    gameDirectory.Add(new VideoGame("Halo", 2030, 3.6));
+    System.Console.WriteLine($"{gameDirectory[0].Name}, {gameDirectory[0].ReleaseYear},{gameDirectory[0].Rating}");
+  }
+}
